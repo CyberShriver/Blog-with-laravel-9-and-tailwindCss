a35a2fb6f77f6f9cf0ec4a606685968bea0d8405
CyberShriver <irakozep18.pi@gmail.com>
2023-02-10 22:26:49 +0200

    retrieve data from DB  with eloquent and display single post in show method

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -35,7 +35,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view('blog.create');
     }
 
     /**
--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -7,26 +7,48 @@
     @vite('resources/css/app.css')
     <title>blog</title>
 </head>
-<body class="container mx-auto px-12">
-    <nav>
-        <a href={{ route('blog.show',['id'=>1]) }} @class(['p-4', 'font-bold' => true])>Goto Blog</a>
-    </nav>
-    <h1 class=" text-center text-2xl font-bold capitalize p-4">Programming not easy at the begining</h1>
-    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, at aspernatur delectus quisquam doloribus, qui quibusdam iusto consequatur nisi,
-         sed ipsum non ratione. Atque, dolores nisi possimus velit distinctio at?</div> 
+<body class="w-full h-full bg-gray-100">
+    <div class="w-4/5 mx-auto">
+        <div class="text-center pt-20">
+            <h1 class="text-3xl text-gray-700">
+                All Articles
+            </h1>
+            <hr class="border border-1 border-gray-300 mt-10">
+        </div>
 
-          {{-- <q>{{$author}}</q>  --}}
-         <div class="text-center font-bold pt-5 underline">lists of posts</div>
+        <div class="py-10 sm:py-20">
+            <a class="primary-btn inline text-base sm:text-xl bg-green-500 py-4 px-4 shadow-xl rounded-full transition-all hover:bg-green-400"
+               href={{route('blog.create')}}>
+                New Article
+            </a>
+        </div>
+    </div>
 
-         <ol class="text-center">
+    @foreach($posts as $post)
+        <div class="w-4/5 mx-auto pb-10">
+            <div class="bg-white pt-10 rounded-lg drop-shadow-2xl sm:basis-3/4 basis-full sm:mr-8 pb-10 sm:pb-0">
+                <div class="w-11/12 mx-auto pb-10">
+                    <h2 class="text-gray-900 text-2xl font-bold pt-6 pb-0 sm:pt-0 hover:text-gray-700 transition-all">
+                        <a href="{{ route('blog.show', $post->id) }}">
+                            {{ $post->title }}
+                        </a>
+                    </h2>
 
-            @forelse ($posts as $post)
-                <li>{{$post->title}}</li>
-            @empty
-                
-            @endforelse
+                    <p class="text-gray-900 text-lg py-8 w-full break-words">
+                        {{ $post->excerpt }}
+                    </p>
 
-
-         </ol>
+                    <span class="text-gray-500 text-sm sm:text-base">
+                    Made by:
+                        <a href=""
+                           class="text-green-500 italic hover:text-green-400 hover:border-b-2 border-green-400 pb-3 transition-all">
+                            Patrick shriver
+                        </a>
+                    on 10-02-2023
+                </span>
+                </div>
+            </div>
+        </div>
+    @endforeach
 </body>
 </html>
