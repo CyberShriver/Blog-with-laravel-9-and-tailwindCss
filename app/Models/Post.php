c3f18f77e1fa6c204e4f18f27b480aebb3cc1e29
CyberShriver <irakozep18.pi@gmail.com>
2023-02-10 17:08:44 +0200

    modify model with eloquant mathod

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $table='posts';
+    protected $primaryKey='title';
+    protected $timestamps=false;
+    protected $dateTime='U';
+    protected $connection='sqlite';
+    protected $attribute=[
+        'is_published'=>true
+    ];
 }
